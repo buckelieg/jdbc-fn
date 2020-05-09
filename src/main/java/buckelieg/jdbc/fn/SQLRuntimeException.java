66a17592df51db1ec3d0477bc4a59c177140d4be
Anatoly <buckelieg@gmail.com>
2020-05-10 02:29:21 +0300

    rollback release

--- src/main/java/buckelieg/jdbc/fn/SQLRuntimeException.java
+++ src/main/java/buckelieg/jdbc/fn/SQLRuntimeException.java
@@ -26,14 +26,19 @@ public class SQLRuntimeException extends RuntimeException {
     private String message;
 
     /**
-     * {@inheritDoc}
+     * Constructs a new runtime exception with {@code null} as its detail message
+     * <br/>The cause is not initialized, and may subsequently be initialized by a call to {@link #initCause}
      */
     public SQLRuntimeException() {
         super();
     }
 
     /**
-     * {@inheritDoc}
+     * Constructs a new runtime exception with the specified detail message
+     * <br/>The cause is not initialized, and may subsequently be initialized by a call to {@link #initCause}
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method
      */
     public SQLRuntimeException(String message) {
         super(message);
@@ -43,7 +48,7 @@ public class SQLRuntimeException extends RuntimeException {
      * Constructs a new runtime exception with provided string as its detail message
      * <br/>If <code>fillInStackTrace</code> parameter is set to <code>true</code> then stack trace is filled in, otherwise - not
      *
-     * @param message exception message string
+     * @param message          exception message string
      * @param fillInStackTrace whether to fill in stack trace or not
      */
     public SQLRuntimeException(String message, boolean fillInStackTrace) {
@@ -54,14 +59,27 @@ public class SQLRuntimeException extends RuntimeException {
     }
 
     /**
-     * {@inheritDoc}
+     * Constructs a new runtime exception with the specified detail message and cause
+     * <p>Note that the detail message associated with {@code cause} is <i>not</i> automatically incorporated in this runtime exception's detail message
+     *
+     * @param message the detail message (which is saved for later retrieval
+     *                by the {@link #getMessage()} method)
+     * @param cause   the cause (which is saved for later retrieval by the
+     *                {@link #getCause()} method).  (A {@code null} value is
+     *                permitted, and indicates that the cause is nonexistent or
+     *                unknown)
      */
     public SQLRuntimeException(String message, Throwable cause) {
         super(message, cause);
     }
 
     /**
-     * {@inheritDoc}
+     * Constructs a new runtime exception with the specified cause and a detail message of {@code (cause==null ? null : cause.toString())}
+     * <br/>(which typically contains the class and detail message of {@code cause})
+     * <br/>This constructor is useful for runtime exceptions that are little more than wrappers for other throwables.
+     *
+     * @param cause the cause (which is saved for later retrieval by the {@link #getCause()} method).
+     *              (A {@code null} value is permitted, and indicates that the cause is nonexistent or unknown)
      */
     public SQLRuntimeException(Throwable cause) {
         super(cause);
